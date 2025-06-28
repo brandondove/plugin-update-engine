8e824b386619f0bfb62dfaac2fbcca0c861e9253
Brandon Dove <brandon@pixeljar.com>
2025-06-27 19:18:52 -0700

    new tags

--- admin-pages/tab-description.php
+++ admin-pages/tab-description.php
@@ -7,12 +7,12 @@
 		<th scope="row">Description</th>
 		<td>
 			<?php $description = ( isset( $pue_plugins[$_REQUEST['id']]['sections']['description'] ) ? $pue_plugins[$_REQUEST['id']]['sections']['description'] : '' ) ?>
-			<?php the_editor( $description ); ?>
+			<?php wp_editor( $description ); ?>
 			<span class="description">Briefly describe what your plugin does.</span>
 		</td>
 	</tr>
-	
+
 	</table>
-	
+
 	<p class="submit"><input type="submit" class="button-primary" value="Save Changes" /></p>
 </div>
--- admin-pages/tab-faq.php
+++ admin-pages/tab-faq.php
@@ -7,12 +7,12 @@
 		<th scope="row">Frequently Asked Questions</th>
 		<td>
 			<?php $faq = ( isset( $pue_plugins[$_REQUEST['id']]['sections']['faq'] ) ? $pue_plugins[$_REQUEST['id']]['sections']['faq'] : '' ) ?>
-			<?php the_editor( $faq ); ?>
+			<?php wp_editor( $faq ); ?>
 			<span class="description">Answer some commonly asked questions here.</span>
 		</td>
 	</tr>
-	
+
 	</table>
-	
+
 	<p class="submit"><input type="submit" class="button-primary" value="Save Changes" /></p>
 </div>
